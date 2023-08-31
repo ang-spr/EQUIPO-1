c21bac2d5121759443af7b25fc28de82f36fd1e4
Sarai Rodriguez <131308548+ang-spr@users.noreply.github.com>
2023-08-31 14:23:12 -0600

    Update Evidencia-1.py

--- Evidencia-1.py
+++ Evidencia-1.py
@@ -1,26 +1,18 @@
 import datetime as dt
 
-print('----TALLER MECANICO DON HAMBLETON---')
-
-print('Buen dia, seleccione la opcion que desee realizar: \n1-Registrar nota \n2-Consulta y reportes')
-print('3-Cancelar una nota \n4-Recuperar una nota \n5-Salir')
-menu = int(input('Opcion: '))
-
 folio = {}
+tupla_servicio = {1:(600,'Cambio de nueomatico'), 2:(900,'Ajuste de vujias'),
+        3:(850,'Cambio de aceite'), 4:(4500,'Reparacion de aceite'), 5:(3000,'Trasmision')}
 
 fecha_actual=dt.date.today()
 
 while True:
+    print('----TALLER MECANICO DON HAMBLETON---')
+    print('Buen dia, seleccione la opcion que desee realizar: \n1-Registrar nota \n2-Consulta y reportes')
+    print('3-Cancelar una nota \n4-Recuperar una nota \n5-Salir')
+    menu = int(input('Opcion: '))
     if menu == 1:
-        tupla_servicio = {1:(600,'Cambio de nueomatico'), 2:(900,'Ajuste de vujias'),
-            3:(850,'Cambio de aceite'), 4:(4500,'Reparacion de aceite'), 5:(3000,'Trasmision')}
         nombre_cliente = input('Ingrese el nombre del cliente: \n')
-       
-        print('Los servicios disponibles son los siguientes: ')
-        print('1-Cambio de neumaticos: $600 \n2-Ajuste de vujias: $900 \n3-Cambio de aceite: $850')
-        print('4-Reparacion de motor: $4500 \n5-Trasmision: $3000')
-        servicio = int(input('Servicio: '))
-        
         while True:
             fecha_registro=input("Ingresar la fecha del sistema (dd/mm/aaaa): ")
             fecha_procesada = dt.datetime.strptime(fecha_registro, "%d/%m/%Y").date()
@@ -28,6 +20,17 @@ while True:
                 print("La fecha es incorrecta. La fecha no debe ser posterior a la actual")  
                 continue
             else: 
+                break
+        
+        while True:
+            print('Los servicios disponibles son los siguientes: ')
+            print('1-Cambio de neumaticos: $600 \n2-Ajuste de vujias: $900 \n3-Cambio de aceite: $850')
+            print('4-Reparacion de motor: $4500 \n5-Trasmision: $3000')
+            servicio = (input('Servicio: '))
+            otro_servicio = input('Desea otro servicio: (si/no) \n')
+            if otro_servicio == 'si':
+                continue
+            elif otro_servicio == 'no':
                 break
 
     clave_folio = max(folio.keys(), default = 0) + 1 
