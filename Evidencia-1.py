c924c79147c94f91e6dc667e2995a8e187b55843
Sarai Rodriguez <131308548+ang-spr@users.noreply.github.com>
2023-08-31 16:05:13 -0600

    Update Evidencia-1.py

--- Evidencia-1.py
+++ Evidencia-1.py
@@ -1,6 +1,6 @@
 import datetime as dt
 
-folio = {}
+nota_final = {}
 tupla_servicio = {1:(600,'Cambio de nueomatico'), 2:(900,'Ajuste de vujias'),
         3:(850,'Cambio de aceite'), 4:(4500,'Reparacion de aceite'), 5:(3000,'Trasmision')}
 
@@ -16,6 +16,7 @@ while True:
         while True:
             fecha_registro=input("Ingresar la fecha del sistema (dd/mm/aaaa): ")
             fecha_procesada = dt.datetime.strptime(fecha_registro, "%d/%m/%Y").date()
+            total = 0
             if fecha_procesada < fecha_actual:       
                 print("La fecha es incorrecta. La fecha no debe ser posterior a la actual")  
                 continue
@@ -27,14 +28,16 @@ while True:
             print('1-Cambio de neumaticos: $600 \n2-Ajuste de vujias: $900 \n3-Cambio de aceite: $850')
             print('4-Reparacion de motor: $4500 \n5-Trasmision: $3000')
             servicio = (input('Servicio: '))
-
+ 
             costo_servicio = float(input("Ingrese el costo del servicio: "))
-            while costo_servicio <= 0:
+            if costo_servicio <= 0:
                 print("El costo debe ser mayor que cero.")
-                costo_servicio = float(input("Ingrese el costo del servicio: "))
-                break
+                continue
+
             otro_servicio = input('Desea otro servicio: (si/no) \n')
+            total += costo_servicio
             if otro_servicio == 'si':
                 continue
             elif otro_servicio == 'no':
+                monto_pagar = print(f'El precio a pagar: ${total}')
                 break
